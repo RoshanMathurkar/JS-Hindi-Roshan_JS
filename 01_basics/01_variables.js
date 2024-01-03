bc1a23bbd0c87e47a26e0b1ff50503c5582355fd
RoshanMathurkar <129658692+RoshanMathurkar@users.noreply.github.com>
2024-01-03 11:50:59 +0000

    container config 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountID = 1234;
+
+let accountName = 'Roahan';
+
+var accountEmail = "test@gmail.com";
+
+accountCity = 'JBP';
+
+let accountState;
+
+//accountID =1; //not allowed, can not assign to const varaible.
+
+accountName = 'Roshan';
+accountEmail ='roshan@gmail.com'
+accountCity = 'AP';
+// console.log(accountName);
+// console.log(accountEmail);
+// console.log(accountCity);
+// console.log(accountState);
+console.table([accountName,accountEmail,accountID,accountCity,accountState])
